95244cc2a134756bf3ada80e6a1bab0f7a7ca76a
Le Gia Luan <legialuan780@gmail.com>
2026-01-09 16:34:44 +0700

    update giao dien ban va edit ban

---
+++ QLCafe.Presentation/Views/Admin/EditTableDialog.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace QLCafe.Presentation.Views.Admin
+{
+    partial class EditTableDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel headerPanel; // changed to Panel (no rounded corners)
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtTableName;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // EditTableDialog
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(520, 220);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Sửa Bàn";
+            this.Controls.Add(this.headerPanel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCancel);
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(55, 142, 241);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Padding = new System.Windows.Forms.Padding(16, 12, 16, 12);
+            this.headerPanel.Size = new System.Drawing.Size(520, 60);
+            this.headerPanel.TabIndex = 0;
+            this.headerPanel.Controls.Add(this.titleLabel);
+            this.headerPanel.Controls.Add(this.closeButton);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.Location = new System.Drawing.Point(16, 16);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Text = "Sửa Bàn";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.closeButton.Text = "×";
+            this.closeButton.Size = new System.Drawing.Size(32, 28);
+            this.closeButton.Location = new System.Drawing.Point(472, 16);
+            this.closeButton.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(24, 80);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 19);
+            this.lblName.Text = "Tên bàn";
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTableName.Location = new System.Drawing.Point(24, 104);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(472, 25);
+            this.txtTableName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTableName.Text = "Bàn 1";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUpdate.Location = new System.Drawing.Point(24, 160);
+            this.btnUpdate.Size = new System.Drawing.Size(220, 36);
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(55, 142, 241);
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancel.Location = new System.Drawing.Point(268, 160);
+            this.btnCancel.Size = new System.Drawing.Size(220, 36);
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(160, 160, 160);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+
+            this.AcceptButton = this.btnUpdate;
+            this.CancelButton = this.btnCancel;
+
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}}
